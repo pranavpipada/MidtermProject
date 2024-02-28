9b67c3e8486b95615f1013c9f21cf25b543e0626
pranavpipada <158802414+pranavpipada@users.noreply.github.com>
2024-02-27 23:44:58 -0500

    Create midtermproject.py
    created file - midtermproject.py and imported json and os. Also defined two functions "read_orders" and "main" function.

---
+++ midtermproject.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+def read_orders(file_path):
+    # Reads orders from example_orders.json file and returns List of orders.
+    with open(file_path, 'r') as orders_file:
+        return json.load(orders_file)
+
+def main():
+    orders_file_path = 'example_orders.json'
+
+    if os.path.exists(orders_file_path):
+        orders = read_orders(orders_file_path)
