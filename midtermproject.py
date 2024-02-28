b4c5a14c1c86232f030f0e193f087c088c692c7a
pranavpipada <158802414+pranavpipada@users.noreply.github.com>
2024-02-28 17:43:09 -0500

    Update midtermproject.py

--- midtermproject.py
+++ midtermproject.py
@@ -6,8 +6,12 @@ def read_orders(file_path):
     with open(file_path, 'r') as orders_file:
         return json.load(orders_file)
 
+
 def main():
     orders_file_path = 'example_orders.json'
 
     if os.path.exists(orders_file_path):
         orders = read_orders(orders_file_path)
+
+    else:
+        print(f"Error: File '{orders_file_path}' not found.")
